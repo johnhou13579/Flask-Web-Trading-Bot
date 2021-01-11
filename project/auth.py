3f0a936c50679db1acc1c38b2102320d0e79d162
Jonathan Hou <thejonathanhou@gmail.com>
2021-01-10 18:26:50 -0800

    Removed Sign up feauture and added Beta feature

--- project/auth.py
+++ project/auth.py
@@ -35,9 +35,10 @@ def login_post():
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
+
     email = request.form.get('email')
     name = request.form.get('name')
-    password = request.form.get('password')
+    #password = request.form.get('password')
 
     user = User.query.filter_by(email = email).first()
 
@@ -45,7 +46,7 @@ def signup_post():
         flash("Email address already exists")
         return redirect(url_for('auth.signup'))
 
-    new_user = User(email=email, name=name, password = generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name)
 
     db.session.add(new_user)
     db.session.commit()
